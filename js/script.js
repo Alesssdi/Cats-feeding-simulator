175e84e9d11404b4ce515dcdf54d49fa83978728
Alesssdi <davydovasasha3@gmail.com>
2024-05-08 23:29:37 +0500

    main

---
+++ js/script.js
@@ -0,0 +1,124 @@
+
+
+
+
+document.getElementById("params-form").addEventListener("submit", function (event) {
+    event.preventDefault(); 
+
+    resetButton = document.createElement("button");
+    resetButton.textContent = "Начать заново";
+    document.getElementById('params-form').appendChild(resetButton);
+    resetButton.classList.add("new_game_button");
+    resetButton.addEventListener("click", resetGame);
+    
+    startButton = document.getElementById("start-btn");
+    startButton.parentNode.removeChild(startButton);
+
+    const n = parseInt(document.getElementById("n").value);
+    const b = parseInt(document.getElementById("b").value);
+    const m = parseInt(document.getElementById("m").value);
+    const t = parseInt(document.getElementById("t").value);
+    const r = parseInt(document.getElementById("r").value);
+
+    if (isNaN(n) || isNaN(b) || isNaN(m) || isNaN(t) || isNaN(r) || n < 1 || b < 1 || m < 1 || t < 1 || r < 1) {
+        alert("Пожалуйста, введите корректные значения.");
+        return;
+    }
+
+    let totalTime = 0;
+    let totalWaitTime = 0;
+    let catsFed = 0;
+    let queue = [];
+    let bowlAmount = m;
+
+
+    async function feedCat() {
+        if (queue.length === 0) return;
+
+        if (bowlAmount==0){
+            await refillBowl();
+        }
+
+        const catIndex = queue.shift();
+        const spd = b / t;
+        totalTime+=t;
+
+        const eatTime = Math.min(bowlAmount, b) / spd;
+        const remains = b - Math.min(bowlAmount, b);
+
+        totalWaitTime = t - eatTime;
+
+        document.getElementById("output").innerText += `Котик ${catIndex + 1} подошел к миске.\n`;
+
+        await new Promise(resolve => {
+            setTimeout(async () => {
+                if (totalWaitTime > 0) {
+                    document.getElementById("output").innerText += `Котик ${catIndex + 1} ждет пока наполнят миску.\n`;
+                    await refillBowl();
+                    document.getElementById("output").innerText += `Котик ${catIndex + 1} продолжил есть.\n`;
+                    bowlAmount -= remains;
+
+                    await new Promise(innerResolve => {
+                        setTimeout(() => {
+                            document.getElementById("output").innerText += `Котик ${catIndex + 1} отошел от миски.\n`;
+                            catsFed++;
+                            innerResolve();
+                        }, totalWaitTime * 1000);
+                    });
+                } else {
+                    bowlAmount -= b;
+                    document.getElementById("output").innerText += `Котик ${catIndex + 1} отошел от миски.\n`;
+                    catsFed++;
+                }
+
+                if (catsFed === n) {
+                    document.getElementById("output").innerText += `Все котики накормлены! Затраченное время: ${totalTime} сек.\n`;
+                } else {
+                    await feedCat();
+                }
+                resolve(); 
+            }, eatTime * 1000);
+        });
+    }
+
+
+    
+    function refillBowl() {
+        document.getElementById("output").innerText += "Бабушка наполняет миску.\n";
+        bowlAmount = m;
+        totalTime += r;
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                document.getElementById("output").innerText += "Миска наполнена.\n";
+                resolve(); 
+            }, r * 1000);
+        });
+    }
+
+
+    for (let i = 0; i < n; i++) {
+        queue.push(i);
+    }
+
+    
+    feedCat();
+
+    function resetGame() {
+        
+        totalTime = 0;
+        totalWaitTime = 0;
+        catsFed = 0;
+        queue = [];
+        bowlAmount = m;
+        document.getElementById("output").innerText = ""; 
+        document.getElementById("params-form").reset();
+        resetButton.parentNode.removeChild(resetButton);
+
+
+        startButton = document.createElement("button");
+        startButton.textContent = "Запустить";
+        document.getElementById('params-form').appendChild(startButton);
+        startButton.classList.add("start-btn");
+        
+    }
+});
